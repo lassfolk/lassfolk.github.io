5ba400240d9e7721f1b2c4639d2c5972f845a825
lassfolk <alf.lassfolk@luukku.com>
2016-10-31 13:33:03 +0200

    Add files via upload

--- sidebar/js/sidebar.js
+++ sidebar/js/sidebar.js
@@ -5,21 +5,17 @@ function toggleNav() {
     if (toggleButton === 0) {
     document.getElementById("sidebar").style.width = "200px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.getElementById("main").style.transition = "0.5s";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    /*document.getElementById("openmenubutton").style.opacity = "0.5";*/
+    document.getElementById("openmenubutton_active").style.opacity = "1";
     document.getElementById("openmenubar").style.color = "blue";
-    document.getElementById("openmenubutton").src="img/menubutton_active.jpg";
     toggleButton = 1;
     }
     else if (toggleButton === 1) {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "200px";
-    document.getElementById("main").style.transition = "0.5s";
     document.body.style.backgroundColor = "rgba(0,0,0,0)";
-    /*document.getElementById("openmenubutton").style.opacity = "1";*/
+    document.getElementById("openmenubutton_active").style.opacity = "0";
     document.getElementById("openmenubar").style.color = "white";
-    document.getElementById("openmenubutton").src="img/menubutton.jpg";
     toggleButton = 0;  
     }
 }
