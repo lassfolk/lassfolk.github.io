0c05d61bf3a6d63fa931ae0fcca4d5bf7a79c1c2
lassfolk <alf.lassfolk@luukku.com>
2016-11-03 11:28:46 +0200

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -16,3 +16,125 @@ window.onclick = function(event) {
     }
   }
 }
+
+/* --- YOUTUBE PLAYERS UNDER THIS LINE --- */
+
+function onYouTubeIframeAPIReady() {
+
+    references = new YT.Player("videobgref", {
+    videoId: "FG0fTKAqZ5g",
+
+    playerVars: {
+        rel: 0,
+        autoplay: 1,
+        controls: 0,
+        showinfo: 0,
+        modestbranding: 1,
+        loop: 1,
+        fs: 0,
+        cc_load_policy: 0,
+        iv_load_policy: 3,
+        autohide: 1,
+        start: 10,
+    },
+    events: {
+      onReady: function(e) {
+        e.target.mute();
+      }
+    }
+  });
+    
+        index = new YT.Player("videobgindex", {
+            
+        videoId: "QI9ta7qkazU",
+        
+        playerVars: {
+        rel: 0,
+        autoplay: 1,
+        controls: 0,
+        showinfo: 0,
+        modestbranding: 1,
+        loop: 1,
+        fs: 0,
+        cc_load_policy: 0,
+        iv_load_policy: 3,
+        autohide: 1,
+        },
+    events: {
+      onReady: function(e) {
+        e.target.mute();
+      }
+    }
+  });
+
+        employment = new YT.Player("videobgemp", {
+            
+        videoId: "lwH3FgbXqMM",
+
+        playerVars: {
+        rel: 0,
+        autoplay: 1,
+        controls: 0,
+        showinfo: 0,
+        modestbranding: 1,
+        loop: 1,
+        fs: 0,
+        cc_load_policy: 0,
+        iv_load_policy: 3,
+        autohide: 1,
+        },
+    events: {
+      onReady: function(e) {
+        e.target.mute();
+      }
+    }
+  });
+        
+        education = new YT.Player("videobgedu", {
+            
+        videoId: "Y_2IK1aGe8U",
+
+        playerVars: {
+        rel: 0,
+        autoplay: 1,
+        controls: 0,
+        showinfo: 0,
+        modestbranding: 1,
+        loop: 1,
+        fs: 0,
+        cc_load_policy: 0,
+        iv_load_policy: 3,
+        autohide: 1,
+        },
+    events: {
+      onReady: function(e) {
+        e.target.mute();
+      }
+    }
+  });   
+    
+        contact = new YT.Player("videobgcont", {
+            
+        videoId: "P5_GlAOCHyE",
+
+        playerVars: {
+        rel: 0,
+        autoplay: 1,
+        controls: 0,
+        showinfo: 0,
+        modestbranding: 1,
+        loop: 1,
+        fs: 0,
+        cc_load_policy: 0,
+        iv_load_policy: 3,
+        autohide: 1,
+        start: 8
+        },
+    events: {
+      onReady: function(e) {
+        e.target.mute();
+      }
+    }
+  });
+    
+ }
